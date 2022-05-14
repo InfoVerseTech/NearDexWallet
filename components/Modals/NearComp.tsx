4b4d08ff1890304cd651fb24c607d6dadf59e4d8
CypherX7 <cypherx702@gmail.com>
2022-05-15 00:39:43 +0530

    near integration

---
+++ components/Modals/NearComp.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import Head from 'next/head';
+import * as nearAPI from "near-api-js";
+const { connect, keyStores, WalletConnection } = nearAPI;
+
+export default function NearComp() {
+  const [Account , SetAccount] = useState();
+  const [Balance , SetBalance] = useState();
+  const TheConfig = () => {
+    const config = {
+      networkId: "testnet",
+      keyStore: (new keyStores.BrowserLocalStorageKeyStore()),
+      nodeUrl: "https://rpc.testnet.near.org",
+      walletUrl: "https://wallet.testnet.near.org",
+      helperUrl: "https://helper.testnet.near.org",
+      explorerUrl: "https://explorer.testnet.near.org",
+    };
+    return config;
+  }
+
+  const ConnectWallet = async() => {
+    const config = TheConfig();
+    const near = await connect(config);
+    const wallet = new WalletConnection(near); 
+    wallet.requestSignIn("senpaitraxh.testnet");
+  }
+
+  const GetAddress = async() => { 
+    const config = TheConfig();
+    const near = await connect(config);
+    const wallet = new WalletConnection(near);
+    const walletAccountId = wallet.getAccountId();
+    SetAccount(walletAccountId);
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>Near Wallet connect</title>
+        <meta name="description" content="Near Wallet Connect" />
+      </Head>
+      <main>
+        <button onClick={ConnectWallet}>Connect</button>
+        <button onClick={GetAddress}>Get Account ID</button>
+        <p>{Account}</p>
+      </main>
+    </div>
+  )
+}
--- pages/Home.tsx
+++ pages/Home.tsx
@@ -5,15 +5,15 @@ import CardTitle from "../components/Home/CardTitle";
 import SwapCard from "../components/Home/SwapCard";
 import ConnectWallet from "../components/Modals/ConnectWallet";
 import SearchBox from "../components/Home/SearchBox";
-
+import NearComp from "../components/Modals/NearComp";
 const Home = () => {
   return (
     <>
       <Header />
       <SearchBox />
+      <NearComp />
       <CardTitle />
       <SwapCard />
-
       <ConnectWallet />
       <Footer />
     </>
